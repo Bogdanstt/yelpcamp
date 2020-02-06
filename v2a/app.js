a5e54f476eea980a33ea435da6f467c219d28b62
Bogdanstt <mym_light@yahoo.com>
2020-02-06 21:21:44 +0200

    pe 6 feb// rest-mongoose working

--- v2a/app.js
+++ v2a/app.js
@@ -5,25 +5,22 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect("mongodb://localhost/yelp_camp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 // SCHEMA SETUP
 let campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 let Campground = new mongoose.model("Campground", campgroundSchema);
 
 // Campground.create({
 //     name: "Lacul Rosu",
-//     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-9FVMx3IU8JwuuthJ2qQzsGxFMLMwEDSVmYwe_vXd2FtmGABXag&s"
+//     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-9FVMx3IU8JwuuthJ2qQzsGxFMLMwEDSVmYwe_vXd2FtmGABXag&s",
+//     description: "This is a beautiful lake, its green not red, so they say."
 //   },
 //   (err, campground) => {
 //     if (err) {
@@ -46,18 +43,6 @@ let campgrounds = [{
   {
     name: "Padurea Neagra",
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWUMDdZ8rT-Vr8Pt7SC9aMqKXruPuO3sb1YpTPYH66CGz74EO-cw&s"
-  },
-  {
-    name: "Salmon Creek",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRqV9A7CCffjsyGnqI8lqo2IaMy0abofUAza_PyYsoluNtBQk5&s"
-  },
-  {
-    name: "Lacul Rosu",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-9FVMx3IU8JwuuthJ2qQzsGxFMLMwEDSVmYwe_vXd2FtmGABXag&s"
-  },
-  {
-    name: "Padurea Neagra",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWUMDdZ8rT-Vr8Pt7SC9aMqKXruPuO3sb1YpTPYH66CGz74EO-cw&s"
   }
 ];
 
@@ -70,7 +55,9 @@ app.get('/campgrounds', (req, res) => {
     if (err) {
       console.log(err);
     } else {
-      res.render("campgrounds", {  campgrounds: allCampgrounds });
+      res.render("index", {
+        campgrounds: allCampgrounds
+      });
     }
   });
 });
@@ -79,14 +66,25 @@ app.get('/campgrounds/new', (req, res) => {
   res.render("new.ejs");
 });
 
+app.get('/campgrounds/:id', (req, res) => {
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("show", {campground: foundCampground});
+    }
+  });
+});
+
 app.post('/campgrounds', (req, res) => {
   let name = req.body.name;
   let image = req.body.image;
+  let desc = req.body.description;
   let newCampground = {
     name: name,
-    image: image
+    image: image,
+    description: desc
   };
-  console.log(req.body.name, req.body.image);
   Campground.create(newCampground, (err, newlyCreated) => {
     if (err) {
       console.log(err);
